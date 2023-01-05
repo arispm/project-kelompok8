bb3005f8813f1f874424ea034d40354719a744c2
adiekisma <adiek.isma@gmail.com>
2023-01-05 12:55:03 +0700

    Update cartshop page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["./index.html", "./cartshop.html", "./checkout.html"],
   theme: {
     extend: {},
   },
